8ff8dfd69b67a83b02ef292f7bf9bb9ab72bd657
SilkySantosh <silkysantosh09@gmail.com>
2022-04-28 11:50:03 +0200

    Add HandlingAlert cypress

---
+++ project2/cypress/integration/HandlingAlerts.ts
@@ -0,0 +1,61 @@
+/// <reference types = 'cypress' />
+
+describe('Handling Alerts', () => { 
+
+     it.only('Interect with Alerts, 1. window:alert', () => {
+
+        cy.visit('https://register.rediff.com/register/register.php')
+        cy.get('[type="submit"]').click()
+
+        // Handeling window:alert
+        //expect(str).to.equal or expect(str).to.contains
+
+         // event with method 'on'
+        cy.on('window:alert',(str) => {
+         //assertions
+         expect(str).to.contains('Your full name cannot be blank.\nPlease enter your firstname and lastname e.g. Sameer Bhagwat')
+        
+        });
+
+      
+   });
+
+    // Handeling window:confirm  You clicked: Ok
+    it.only('Interect with Alerts, 2. window:confirm You clicked: Ok', () => {
+
+      cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
+      cy.get(':nth-child(2) > button').click()
+
+
+      cy.on('window:confirm', (str) => {
+         expect(str).to.equal('I am a JS Confirm')
+      })
+
+
+    });
+
+     // Handeling window:confirm  You clicked: Cancle
+
+    it.only('Interect with Alerts, 3. window:confirm You clicked: Cancle', () => {
+
+      cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
+      
+      cy.on('window:confirm', (str) => {
+         return false;
+      })
+      
+      // click on Click for JS Confirm button
+      cy.get(':nth-child(2) > button').click()
+      
+       // verify application message on Cancel button click
+      cy.get('#result').should('have.text','You clicked: Cancel')
+
+
+
+      
+      
+
+    })
+    
+
+})
